3106548b4ceba2e09d268dd6a8fe32d3444d9feb
pankajb1sht <pankajb1sht@proton.me>
2024-06-13 17:45:18 +0530

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    
+
+    // Uncomment this block to pass the first stage
+    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
